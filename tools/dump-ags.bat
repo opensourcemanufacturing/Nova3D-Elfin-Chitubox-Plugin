12643804ce4dc34567e84319266a4f83fc818908
X3msnake <X3msnake@users.noreply.github.com>
2019-11-19 12:24:51 +0000

    Update dump-ags.bat

--- tools/dump-ags.bat
+++ tools/dump-ags.bat
@@ -1,6 +1,8 @@
-@rem :: Batch file to dump the passed args to a text file ::
-@rem :: echo prints all passed args with %*               ::
-@rem :: And >> to append args to the declared file        ::
+@rem :: Batch file to dump the passed args to a text file        ::
+@rem :: echo prints all passed args with %*                      ::
+@rem :: And >> to append args to the declared file               ::
+@rem :: For loop recursively reads the files and logs them       ::
 
 @echo off
 echo %* >> args-log.txt
+for /r %%i in (*) do echo %%i >> args-log.txt
