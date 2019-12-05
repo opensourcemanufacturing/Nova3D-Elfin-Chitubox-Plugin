d37ea0ffa3b44ebfa3baf3bb576e99d5cceb2109
X3msnake <X3msnake@users.noreply.github.com>
2019-12-05 19:47:24 +0000

    Update plugin.bat

--- Nova3D-PluginSource/plugin.bat
+++ Nova3D-PluginSource/plugin.bat
@@ -1,2 +1,4 @@
 cd /D "%~dp0"
-java -jar LD001plugin.jar %*
+java -jar LD001plugin.jar %* 2>> log.txt
+
+REM https://stackoverflow.com/questions/13215348/how-to-output-java-jar-errors-to-a-text-log-file
